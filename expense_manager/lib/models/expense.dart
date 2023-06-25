7b11efbdb8c059c36b855fe326b53202227b2e40
ilakshaygupt <99826011+ilakshaygupt@users.noreply.github.com>
2023-06-26 01:26:47 +0530

    created data converting method

--- expense_manager/lib/models/expense.dart
+++ expense_manager/lib/models/expense.dart
@@ -1,9 +1,8 @@
-import 'package:uuid/uuid.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
 
 const uuid = Uuid();
-
 final formatter = DateFormat.yMd();
 
 enum Category { food, transport, bills, entertainment, other }
@@ -17,30 +16,59 @@ const categoryIcons = {
 };
 
 class Expense {
-  Expense(
-      {required this.title,
-      required this.amount,
-      required this.date,
-      required this.category})
-      : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
   final String id;
   final String title;
   final double amount;
   final DateTime date;
   final Category category;
-  get formattedDate {
+
+  String get formattedDate {
     return formatter.format(date);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'date': date.toIso8601String(),
+      'category': category.toString().split('.').last,
+    };
+  }
+
+  factory Expense.fromJson(Map<String, dynamic> json) {
+    return Expense(
+      title: json['title'],
+      amount: json['amount'],
+      date: DateTime.parse(json['date']),
+      category: Category.values.firstWhere(
+        (category) => category.toString().split('.').last == json['category'],
+      ),
+    );
   }
 }
 
 class ExpenseBucket {
-  const ExpenseBucket({required this.category, required this.expenses});
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
   ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
       : expenses = allExpenses
             .where((expense) => expense.category == category)
-            .toList(); //additional constructor function to class
+            .toList();
+
   final Category category;
   final List<Expense> expenses;
+
   double get totalExpenses {
     double sum = 0;
     for (final expense in expenses) {
